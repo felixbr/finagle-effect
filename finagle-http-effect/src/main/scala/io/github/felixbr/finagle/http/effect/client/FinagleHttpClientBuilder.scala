11c51aaada3615feba6c25fcf4d74efcc36d6594
Felix Bruckmeier <felix.m.bruckmeier@gmail.com>
2020-03-21 17:06:59 +0100

    Make finagle initialization respect laziness semantics

--- finagle-http-effect/src/main/scala/io/github/felixbr/finagle/http/effect/client/FinagleHttpClientBuilder.scala
+++ finagle-http-effect/src/main/scala/io/github/felixbr/finagle/http/effect/client/FinagleHttpClientBuilder.scala
@@ -1,9 +1,10 @@
 package io.github.felixbr.finagle.http.effect.client
 
 import cats.effect._
-import com.twitter.finagle.{Http, Name, Service}
+import cats.syntax.functor._
 import com.twitter.finagle.Http.Client
 import com.twitter.finagle.http.{Request, Response}
+import com.twitter.finagle.{Http, Name, Service}
 import io.github.felixbr.finagle.core.effect.WrappedService
 
 import scala.concurrent.duration._
@@ -11,16 +12,16 @@ import scala.concurrent.duration._
 object FinagleHttpClientBuilder {
 
   def apply[F[_]: Sync: Async: ContextShift]: FinagleHttpClientBuilder[F] =
-    new FinagleHttpClientBuilder[F]
+    new FinagleHttpClientBuilder[F](Sync[F].delay(Http.client), closeTimeout = 1.second)
 }
 
-final case class FinagleHttpClientBuilder[F[_]: Sync: Async: ContextShift](
-  private val underlyingBuilder: Client = Http.client,
-  private val closeTimeout: FiniteDuration = 1.second
+final private[client] case class FinagleHttpClientBuilder[F[_]: Sync: Async: ContextShift](
+  private val underlyingBuilder: F[Client],
+  private val closeTimeout: FiniteDuration
 ) {
 
   def withUpdatedConfig(f: Client => Client): FinagleHttpClientBuilder[F] =
-    copy(underlyingBuilder = f(underlyingBuilder))
+    copy(underlyingBuilder = underlyingBuilder.map(f))
 
   def withCloseTimeout(duration: FiniteDuration): FinagleHttpClientBuilder[F] =
     copy(closeTimeout = duration)
@@ -28,27 +29,34 @@ final case class FinagleHttpClientBuilder[F[_]: Sync: Async: ContextShift](
   def serviceResource(
     dest: String,
   ): Resource[F, WrappedService[F, Request, Response]] =
-    makeServiceResource(underlyingBuilder.newService(dest), closeTimeout)
+    makeServiceResource(
+      underlyingBuilder.map(_.newService(dest)),
+      closeTimeout
+    )
 
   def serviceResource(
     dest: String,
     label: String,
   ): Resource[F, WrappedService[F, Request, Response]] =
-    makeServiceResource(underlyingBuilder.newService(dest, label), closeTimeout)
+    makeServiceResource(
+      underlyingBuilder.map(_.newService(dest, label)),
+      closeTimeout
+    )
 
   def serviceResource(
     dest: Name,
     label: String,
   ): Resource[F, WrappedService[F, Request, Response]] =
-    makeServiceResource(underlyingBuilder.newService(dest, label), closeTimeout)
+    makeServiceResource(
+      underlyingBuilder.map(_.newService(dest, label)),
+      closeTimeout
+    )
 
   private def makeServiceResource(
-    newService: Service[Request, Response],
+    newService: F[Service[Request, Response]],
     closeTimeout: FiniteDuration
   ): Resource[F, WrappedService[F, Request, Response]] =
     Resource.make {
-      Sync[F].delay {
-        new WrappedService[F, Request, Response](newService)
-      }
+      newService.map(new WrappedService[F, Request, Response](_))
     }(service => service.close(closeTimeout))
 }
